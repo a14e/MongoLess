fbf946580258ce1fdf0e0be3bd313da276775eeb
Andrew <m0hct3r@gmail.com>
2018-01-08 02:48:11 +0300

    * typo

--- src/test/scala/a14e/bson/encoder/GenericBsonEncodersSpec.scala
+++ src/test/scala/a14e/bson/encoder/GenericBsonEncodersSpec.scala
@@ -13,6 +13,13 @@ case class SampleUser(id: ID[Int],
 case class Job(company: String,
                salary: Long)
 
+case class Level(levelNumber: Int,
+                 nextLevel: Option[Level])
+
+
+case class NamedNode(nodeName: String,
+                     children: Map[String, NamedNode])
+
 class GenericBsonEncodersSpec extends FlatSpec with Matchers {
 
   "GenericEncoder" should "encode simple class" in {
@@ -58,7 +65,7 @@ class GenericBsonEncodersSpec extends FlatSpec with Matchers {
     user.asBson shouldBe expectedBson
   }
 
-  it should "encode recourcive classes" in {
+  it should "encode recursive classes" in {
     val user = SampleUser(
       id = 213,
       name = "name",
@@ -86,5 +93,50 @@ class GenericBsonEncodersSpec extends FlatSpec with Matchers {
     )
 
     user.asBson shouldBe expectedBson
+  }
+
+  it should "encode recursive option classes" in {
+    val level = Level(
+      levelNumber = 1,
+      nextLevel = Some(
+        Level(
+          levelNumber = 2,
+          nextLevel = None
+        )
+      )
+    )
+    val expectedBson = Bson.obj(
+      "levelNumber" -> 1,
+      "nextLevel" -> Bson.obj(
+        "levelNumber" -> 2
+      )
+    )
+
+    level.asBson shouldBe expectedBson
+  }
+
+  it should "encode recursive map classes" in {
+    val node =
+      NamedNode(
+        nodeName = "node1",
+        children = Map(
+          "node2" -> NamedNode("node2", Map.empty),
+          "node3" -> NamedNode("node3", Map.empty)
+        )
+      )
+    val expectedBson = Bson.obj(
+      "nodeName" -> "node1",
+      "children" -> Bson.obj(
+        "node2" -> Bson.obj(
+          "nodeName" -> "node2",
+          "children" -> Bson.obj()
+        ),
+        "node3" -> Bson.obj(
+          "nodeName" -> "node3",
+          "children" -> Bson.obj()
+        )
+      )
+    )
+    node.asBson shouldBe expectedBson
   }
 }
